5246b2d6bfd4a7bf6bc6cd8589de3484ad8fbc38
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-22 13:26:49 +0200

    test add expense action object value

--- src/test/actions/expenses.test.js
+++ src/test/actions/expenses.test.js
@@ -1,4 +1,4 @@
-import {editExpense, removeExpense} from "../../actions/expenses";
+import {addExpense, editExpense, removeExpense} from "../../actions/expenses";
 
 // test REMOVE object
 test('should setup remove expense action object', () => {
@@ -11,10 +11,46 @@ test('should setup remove expense action object', () => {
 
 // test EDIT object
 test('should setup edit expense action object', () => {
-    const action = editExpense({ id: '123abc', update: '' });
+    const action = editExpense('123abc', { note: 'test edit note' });
     expect(action).toEqual({
         type: 'EDIT_EXPENSE',
         id: '123abc',
-        update: ''
+        update: {
+            note: 'test edit note'
+        }
+    })
+});
+
+// test add expense action object with seed
+test('should setup add expense with seed value', () => {
+    const seed = {
+        description: 'Rent',
+        amount: 100,
+        createdAt: 1000,
+        note: 'pay rent'
+    };
+
+    const action = addExpense(seed);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expenses: {
+            ...seed,
+            id: expect.any(String)
+        }
+    });
+});
+
+// test add expense action object with default value
+test('should setup add expense with default value', () => {
+    const action = addExpense();
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expenses: {
+            description: '',
+            note: '',
+            amount: 0,
+            createdAt: 0,
+            id: expect.any(String)
+        }
     })
 });
