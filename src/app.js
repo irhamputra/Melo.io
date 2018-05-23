09f8ac3c54d08a7a214f82006fb0f20c44ed9cf6
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-23 11:11:47 +0200

    remove dummy testing data

--- src/app.js
+++ src/app.js
@@ -8,22 +8,7 @@ import { Provider } from 'react-redux';
 // Router
 import Routes from './routers/AppRouter';
 
-// Redux
-import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
-import getVisibleExpenses from './selectors/expenses';
-
 const store = configStore();
-
-// mock data. TODO: connect to Databases and remove mock data
-store.dispatch(addExpense({ description: 'Spotify', amount: 900, createdAt: 1526949601009 }));
-store.dispatch(addExpense({ description: 'Pay Rent', amount: 400, createdAt: 1526949582329}));
-store.dispatch(setTextFilter());
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
 
 const App = () => (
     <Provider store={store}>
