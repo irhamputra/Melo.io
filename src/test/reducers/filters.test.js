efd3e995dc356f9cf1bf913c8eb8ef54e7c59db2
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-23 10:00:10 +0200

    test filter reducers

---
+++ src/test/reducers/filters.test.js
@@ -0,0 +1,12 @@
+import moment from 'moment';
+import filtersReducer from '../../reducers/filters';
+
+test('should setup filter values', () => {
+    const state = filtersReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual({
+        text: '',
+        sortBy: 'date',
+        startDate: moment().startOf('month'),
+        endDate: moment().endOf('month')
+    })
+});
