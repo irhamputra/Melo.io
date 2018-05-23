7adf8a02f5d390e0ade574b88be8a3026b50ece4
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-23 16:06:44 +0200

    set new features invoice total and reformat amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt, note }) => {
     return (
@@ -7,9 +9,9 @@ const ExpenseListItem = ({ dispatch, id, description, amount, createdAt, note })
             <Link to={`/edit/${id}`}>
                 <h3>{description}</h3>
             </Link>
-            <p>€{amount}</p>
-            {note && <p>note: {note}</p>}
-            <p>created at: {createdAt}</p>
+            <p>{numeral(amount / 100).format('$0,0.00')}</p>
+            {note && <p>Note: {note}</p>}
+            <p>{moment(createdAt).format('MMMM Do, YYYY')}</p>
         </div>
     )
 };
