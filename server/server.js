7615c0f11e20a1687964ec56809071594d010045
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-23 14:04:56 +0200

    setup prod build and server

--- server/server.js
+++ server/server.js
@@ -3,14 +3,12 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 3004 || process.env.PORT;
+const PORT = 3030 || process.env.PORT;
 
-app.use(express.static(path.join(__dirname, '..', '/public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, '..', 'public','index.html'))
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at localhost:${PORT}`)
-});
+app.listen(PORT, () => console.log(`Server running localhost:${PORT}`))
