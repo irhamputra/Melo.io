6f089483fb55f1d3abefe3101209e8602cad392b
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-23 10:12:57 +0200

    test SORT_BY_DATE and SORT_BY_AMOUNT filter reducer object

--- src/test/reducers/filters.test.js
+++ src/test/reducers/filters.test.js
@@ -1,12 +1,22 @@
 import moment from 'moment';
-import filtersReducer from '../../reducers/filters';
+import filterReducer from '../../reducers/filters';
 
 test('should setup filter values', () => {
-    const state = filtersReducer(undefined, { type: '@@INIT' });
+    const state = filterReducer(undefined, { type: '@@INIT' });
     expect(state).toEqual({
         text: '',
         sortBy: 'date',
         startDate: moment().startOf('month'),
         endDate: moment().endOf('month')
     })
+});
+
+test('should sort by amount', () => {
+    const state = filterReducer(undefined, { type: 'SORT_BY_AMOUNT'});
+    expect(state.sortBy).toBe('amount');
+});
+
+test('should sort by date', () => {
+    const state = filterReducer(undefined, { type: 'SORT_BY_DATE' });
+    expect(state.sortBy).toBe('date');
 });
