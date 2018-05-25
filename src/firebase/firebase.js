7f0b9479d1e51c285cff496ceb12316f48ef5175
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-25 15:02:45 +0200

    firebase init

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: "https://melo-io-test.firebaseio.com",
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
