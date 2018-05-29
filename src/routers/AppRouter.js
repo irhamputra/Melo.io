7c6e6d285e453a7f74ea55137aa9e85535a710ce
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-29 11:51:33 +0200

    LoginPage init

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,13 +6,15 @@ import IndexPage from '../components/Index';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
+import LoginPage from "../components/LoginPage";
 
 // Add Route Here!!!
 const Routes = () => {
     return(
         <HashRouter>
             <Switch>
-                <Route exact path="/" component={IndexPage}/>
+                <Route exact path="/" component={LoginPage}/>
+                <Route path="/dashboard" component={IndexPage}/>
                 <Route path="/create" component={AddExpensePage}/>
                 <Route path="/edit/:id" component={EditExpensePage}/>
                 <Route path="/help" component={HelpPage}/>
